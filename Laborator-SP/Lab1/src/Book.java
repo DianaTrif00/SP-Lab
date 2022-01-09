6dc56fa41604ac4db9d7dd7baf37ed8cd21739b1
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-01-09 11:43:39 +0200

    Add files via upload

---
+++ Laborator-SP/Lab1/src/Book.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    public String name;
+    public List<String> paragrafe;
+    public List<String> imagini;
+    public List<String> tabel;
+
+    public Book(String name) {
+        this.name = name;
+        this.paragrafe = new ArrayList();
+        this.imagini = new ArrayList();
+        this.tabel = new ArrayList();
+    }
+
+    public void createNewParagraph(String paragraf) {
+        this.paragrafe.add(paragraf);
+    }
+
+    public void createNewImage(String imagine) {
+        this.imagini.add(imagine);
+    }
+
+    public void createNewTable(String tabele) {
+        this.tabel.add(tabele);
+    }
+
+    public void print() {
+        System.out.println(this.paragrafe);
+        System.out.println(this.imagini);
+        System.out.println(this.tabel);
+    }
+}
