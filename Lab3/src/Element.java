f8e7c7ddc6635129352b992ca7133c176989da10
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:36:13 +0200

    Update Element.java

--- Lab3/src/Element.java
+++ Lab3/src/Element.java
@@ -1,6 +1,12 @@
 public interface Element {
+    default void add(Element element){
+        throw new UnsupportedOperationException("Don't add.");
+    };
+    default void remove(Element element){
+        throw new UnsupportedOperationException("Don't remove.");
+    };
+    default Element get(Integer index){
+        throw new UnsupportedOperationException("Don't get.");
+    };
     void print();
-    void add(Element element) throws Exception;
-    void remove(Element element);
-    boolean find(Element element);
 }
