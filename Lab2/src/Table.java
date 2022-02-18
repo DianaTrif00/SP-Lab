14a9f7de6d0738d4a2648b08e0871ebf30d80095
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:31:57 +0200

    Update Table.java

--- Lab2/src/Table.java
+++ Lab2/src/Table.java
@@ -1,12 +1,18 @@
-public class Table implements Element {
-    private String title;
+public class Table implements Element{
+    String title;
 
     public Table(String title) {
         this.title = title;
     }
-
+    @Override
     public void print() {
         System.out.println(this.title);
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String newTitle){
+        this.title = newTitle;
     }
 }
 
