e4a67a2d2bd6f033fd323d3412800838510f4c6d
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:33:59 +0200

    Update Author.java

--- Lab3/src/Author.java
+++ Lab3/src/Author.java
@@ -1,12 +1,17 @@
-public class Author {
-    private String name;
+public class Author{
+    String name;
 
     public Author(String name) {
         this.name = name;
     }
-
-    public void print() {
-        System.out.println(this.name);
+    public void print(){
+        System.out.println("Author: " + this.name);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newName){
+        this.name = newName;
     }
 }
 
