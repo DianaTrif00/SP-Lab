7db9b7b9a2343bd652fd28e0e7a5e898f7d2f87c
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:48:14 +0200

    Create ImageContent.java

---
+++ Lab4/src/ImageContent.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+
+public class ImageContent {
+    BufferedImage image;
+
+    public ImageContent(BufferedImage image){
+        if(this.image == null) {
+            this.image = image;
+        }
+    }
+
+    public BufferedImage getBufferedImage(){
+        return image;
+    }
+}
