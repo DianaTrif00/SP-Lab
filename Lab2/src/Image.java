d0b5cc34d1ea0ae42dd88e3dfcf3c3f5a6b5c9e1
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:29:42 +0200

    Update Image.java

--- Lab2/src/Image.java
+++ Lab2/src/Image.java
@@ -1,11 +1,17 @@
-public class Image implements Element {
-    private String imageName;
+public class Image implements Element{
+    String url;
 
-    public Image(String imageName) {
-        this.imageName = imageName;
+    public Image(String url) {
+        this.url = url;
     }
-
+    @Override
     public void print() {
-        System.out.println(this.imageName);
+        System.out.println(this.url);
+    }
+    public String getUrl(){
+        return this.url;
+    }
+    public void setUrl(String newUrl){
+        this.url = newUrl;
     }
 }
