eaedb887896693454123367c61ae434c0dd5f482
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:45:23 +0200

    Update Image.java

--- Lab4/src/Image.java
+++ Lab4/src/Image.java
@@ -1,58 +1,25 @@
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-
 public class Image implements Element, Picture {
-    private String imageName;
-    private Dimension dim = new Dimension(400,400);
+    private String url;
+    private ImageContent content = null;
 
-    public Image(String imageName) {
-        this.imageName = imageName;
-
-        try
-        {
-            TimeUnit.SECONDS.sleep(5);
-        }
-        catch( InterruptedException e) { e.printStackTrace(); }
+    public Image(String url) {
+        this.url = url;
     }
-
+    @Override
+    public ImageContent content(){
+        return content;
+    }
+    public String getImgUrl(){
+        return this.url;
+    }
+    @Override
     public void print() {
-        System.out.println(this.imageName);
+        System.out.println(this.url);
     }
-
-    @Override
-    public void add(Element element) {
-        //not needed
+    public String getUrl(){
+        return this.url;
     }
-
-
-    @Override
-    public void remove(Element element) {
-        //not needed
-    }
-
-    @Override
-    public boolean find(Element element) {
-        if (!(element instanceof Image))
-            return false;
-        else {
-            return ((Image) element).imageName.equals(this.imageName);
-        }
-    }
-
-    @Override
-    public Element clone() {
-        Image newimage = new Image(this.imageName);
-        return newimage;
-    }
-
-    @Override
-    public String url() {
-        return this.imageName;
-    }
-
-    @Override
-    public Dimension dim() {
-        return this.dim;
+    public void setUrl(String newImgName){
+        this.url = newImgName;
     }
 }
-
