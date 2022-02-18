544514a3dda40eaf88ae24c0b6f515cb27510e69
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:38:18 +0200

    Update Section.java

--- Lab3/src/Section.java
+++ Lab3/src/Section.java
@@ -1,42 +1,33 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class Section implements Element {
-    private String title;
-    private ArrayList<Element> elements;
+public class Section implements Element{
+    String title;
+    List<Element> elementList = new ArrayList<>();
 
     public Section(String title) {
         this.title = title;
-        this.elements = new ArrayList<Element>();
     }
 
     @Override
-    public void add(Element element) throws Exception {
-        for (Element i : this.elements)
-            if (i.find(element))
-                throw new Exception("Unsupported Operation");
-
-        this.elements.add(element);
+    public void add(Element e) {
+        elementList.add(e);
     }
 
     @Override
-    public void remove(Element element) {
-        this.elements.remove(element);
-    }
-
-    @Override
-    public boolean find(Element element) {
-        for (Element i : this.elements) {
-            if (i.find(element))
-                return true;
-        }
-
-        return false;
-    }
-
     public void print() {
-        System.out.println("Titlu: " + this.title);
-
-        for (Element i : this.elements)
-            i.print();
+        System.out.println(this.title);
+        for (Element e: this.elementList){
+            e.print();
+        }
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public List<Element> getElementList(){
+        return this.elementList;
+    }
+    public void setTitle(String newTitle){
+        this.title = newTitle;
     }
 }
