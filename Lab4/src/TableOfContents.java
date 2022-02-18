937a8f20e4705e1338ab92f8762c4f5780e16c74
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:54:22 +0200

    Update TableOfContents.java

--- Lab4/src/TableOfContents.java
+++ Lab4/src/TableOfContents.java
@@ -1,34 +1,19 @@
+
 public class TableOfContents implements Element {
-    private String title;
+        String title;
 
-    public TableOfContents(String title)
-    {
-        this.title=title;
-    }
+        public TableOfContents(String title) {
+            this.title = title;
+        }
 
-    public void print() {
-        System.out.println("Cuprins");
+        @Override
+        public void print(){
+            System.out.println(this.title);
+        }
+        public String getTitle(){
+            return this.title;
+        }
+        public void setTitle(String newTitle){
+            this.title = title;
+        }
     }
-
-    @Override
-    public void add(Element element) {
-        //not needed
-    }
-
-    @Override
-    public void remove(Element element) {
-        //not needed
-    }
-
-    @Override
-    public boolean find(Element element) {
-        //not needed
-        return false;
-    }
-
-    @Override
-    public Element clone() {
-        TableOfContents newtoc=new TableOfContents(this.title);
-        return newtoc;
-    }
-}
