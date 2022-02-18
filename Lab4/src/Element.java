25ae9f4405f4a2d94b3949c953482c16a5b657cf
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:44:56 +0200

    Update Element.java

--- Lab4/src/Element.java
+++ Lab4/src/Element.java
@@ -1,8 +1,12 @@
-import java.util.ArrayList;
 public interface Element {
+    default void add(Element element){
+        throw new UnsupportedOperationException("Don't add.");
+    };
+    default void remove(Element element){
+        throw new UnsupportedOperationException("Don't remove.");
+    };
+    default Element get(Integer index){
+        throw new UnsupportedOperationException("Don't get.");
+    };
     void print();
-    void add(Element element) throws Exception;
-    void remove(Element element);
-    boolean find(Element element);
-    Element clone();
 }
