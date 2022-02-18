4009a33dbab1e3c4a12d7e94a9ca4db528eee6b5
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2022-02-18 16:30:24 +0200

    Update Main.java

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,22 +1,21 @@
 public class Main {
     public Main() {
     }
-
     public static void main(String[] args) {
         Book discoTitanic = new Book("Disco Titanic");
         Author rpGheo = new Author("Radu Pavel Gheo");
         discoTitanic.addAuthor(rpGheo);
-        int indexChapterOne = discoTitanic.addChapter("Capitolul 1");
-        Chapter chp1 = discoTitanic.getChapterByIndex(indexChapterOne);
-        int indexSubChapterOneOne = chp1.addSubChapter("Subcapitolul 1.1");
-        SubChapter scOneOne = chp1.getSubChapterByIndex(indexSubChapterOneOne);
-        scOneOne.addElement(new Paragraph("Paragraph 1"));
-        scOneOne.addElement(new Paragraph("Paragraph 2"));
-        scOneOne.addElement(new Paragraph("Paragraph 3"));
-        scOneOne.addElement(new Image("Image 1"));
-        scOneOne.addElement(new Paragraph("Paragraph 4"));
-        scOneOne.addElement(new Table("Table 1"));
-        scOneOne.addElement(new Paragraph("Paragraph 5"));
+        int indexChapterOne = discoTitanic.createChapter("Capitolul 1");
+        Chapter chp1 = discoTitanic.getChapter(indexChapterOne);
+        int indexSubChapterOneOne = chp1.createSubChapter("Subcapitolul 1.1");
+        SubChapter scOneOne = chp1.getSubChapter(indexSubChapterOneOne);
+        scOneOne.createNewParagraph("Paragraph 1");
+        scOneOne.createNewParagraph("Paragraph 2");
+        scOneOne.createNewParagraph("Paragraph 3");
+        scOneOne.createNewImage("Image 1");
+        scOneOne.createNewParagraph("Paragraph 4");
+        scOneOne.createNewTable("Table 1");
+        scOneOne.createNewParagraph("Paragraph 5");
         scOneOne.print();
     }
 }
